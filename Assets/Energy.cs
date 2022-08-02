1f480a37540cfddb2a3162e4a735226c7878c37d
Altug Parlak <alparlak@hotmail.com>
2022-08-02 19:10:12 +0300

    PostProcessing Glow Effect

---
+++ Assets/Energy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour
+{
+
+    [SerializeField] float rotateSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, 6.0f * rotateSpeed * Time.deltaTime);
+    }
+
+
+}
