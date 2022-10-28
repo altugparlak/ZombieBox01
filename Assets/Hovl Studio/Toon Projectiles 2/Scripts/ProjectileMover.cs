0c50bf62640c5b3475792ed4b322c2e067b0e1a0
Altug Parlak <alparlak@hotmail.com>
2022-10-28 10:22:01 +0300

    Propeller&NewShootingEffects

--- Assets/Hovl Studio/Toon Projectiles 2/Scripts/ProjectileMover.cs
+++ Assets/Hovl Studio/Toon Projectiles 2/Scripts/ProjectileMover.cs
@@ -46,41 +46,41 @@ public class ProjectileMover : MonoBehaviour
 	}
 
     //https ://docs.unity3d.com/ScriptReference/Rigidbody.OnCollisionEnter.html
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    //Lock all axes movement and rotation
-    //    rb.constraints = RigidbodyConstraints.FreezeAll;
-    //    speed = 0;
+    void OnCollisionEnter(Collision collision)
+    {
+        //Lock all axes movement and rotation
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        speed = 0;
 
-    //    ContactPoint contact = collision.contacts[0];
-    //    Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
-    //    Vector3 pos = contact.point + contact.normal * hitOffset;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point + contact.normal * hitOffset;
 
-    //    if (hit != null)
-    //    {
-    //        var hitInstance = Instantiate(hit, pos, rot);
-    //        if (UseFirePointRotation) { hitInstance.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180f, 0); }
-    //        else if (rotationOffset != Vector3.zero) { hitInstance.transform.rotation = Quaternion.Euler(rotationOffset); }
-    //        else { hitInstance.transform.LookAt(contact.point + contact.normal); }
+        if (hit != null)
+        {
+            var hitInstance = Instantiate(hit, pos, rot);
+            if (UseFirePointRotation) { hitInstance.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(0, 180f, 0); }
+            else if (rotationOffset != Vector3.zero) { hitInstance.transform.rotation = Quaternion.Euler(rotationOffset); }
+            else { hitInstance.transform.LookAt(contact.point + contact.normal); }
 
-    //        var hitPs = hitInstance.GetComponent<ParticleSystem>();
-    //        if (hitPs != null)
-    //        {
-    //            Destroy(hitInstance, hitPs.main.duration);
-    //        }
-    //        else
-    //        {
-    //            var hitPsParts = hitInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
-    //            Destroy(hitInstance, hitPsParts.main.duration);
-    //        }
-    //    }
-    //    foreach (var detachedPrefab in Detached)
-    //    {
-    //        if (detachedPrefab != null)
-    //        {
-    //            detachedPrefab.transform.parent = null;
-    //        }
-    //    }
-    //    Destroy(gameObject);
-    //}
+            var hitPs = hitInstance.GetComponent<ParticleSystem>();
+            if (hitPs != null)
+            {
+                Destroy(hitInstance, hitPs.main.duration);
+            }
+            else
+            {
+                var hitPsParts = hitInstance.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(hitInstance, hitPsParts.main.duration);
+            }
+        }
+        foreach (var detachedPrefab in Detached)
+        {
+            if (detachedPrefab != null)
+            {
+                detachedPrefab.transform.parent = null;
+            }
+        }
+        Destroy(gameObject);
+    }
 }
